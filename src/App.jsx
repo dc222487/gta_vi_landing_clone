467c7e44538c7a39a0c71245434ce4e0cf7ac443
dolphin sam <dc222487@gmail.com>
2025-10-10 09:32:48 +1100

    Implement postcard

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import FirstVideo from './sections/FirstVideo';
 import Jason from './sections/Jason';
 import SecondVideo from './sections/SecondVideo';
 import Lucia from './sections/Lucia';
+import PostCard from './sections/PostCard';
 
 const App = () => {
   return (
@@ -21,6 +22,8 @@ const App = () => {
 
        <SecondVideo/>
        <Lucia/>
+
+       <PostCard/>
     </main>
   )
 }
